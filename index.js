715e2a87cd7252c37f3053548f9c621ddece54e6
theHimanshuShekhar <hshekhar.dev@gmail.com>
2018-12-03 04:31:18 +0530

    static webpage

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client({
+    disableEveryone: true
+});
 bot.commands = new Discord.Collection;
 
 fs.readdir("./commands/", (err, files) => {
     console.clear();
-    if(err) console.log(err);
+    if (err) console.log(err);
     let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if(jsfile.length <= 0) {
+    if (jsfile.length <= 0) {
         console.log("Could not find commands.");
         return;
     }
@@ -19,21 +21,33 @@ fs.readdir("./commands/", (err, files) => {
     });
 });
 
-bot.on("ready", async() => {
+bot.on("ready", async () => {
     console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-    bot.user.setActivity(">>help", {type: "WATCHING"});
+    bot.user.setActivity(">>help", {
+        type: "WATCHING"
+    });
 });
 
 bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
 
     let prefix = process.env.PREFIX;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
-    if(commandfile) commandfile.run(bot, message, args);
+    if (commandfile) commandfile.run(bot, message, args);
 });
 
 bot.login(process.env.TOKEN);
+
+// Webserver
+var http = require('http');
+
+var server = http.createServer((request, response) => {
+    response.write("<h1>Bhayanak Bot</h1>")
+    response.write("<img src='https://i.imgur.com/CIAJAgg.jpg'>")
+});
+
+server.listen(process.env.PORT || 8080);
