9bf563df8822913e52c8471c4089736c6f693f01
thehshekhar <hshekhar.dev@gmail.com>
2018-12-08 19:17:59 +0530

    changed serverinfo formatting

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,18 +1,26 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+    let options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    };
     let createDate = message.guild.createdAt.toLocaleString("en-IN", options);
     let joinDate = message.member.joinedAt.toLocaleString("en-IN", options);
     let serverembed = new Discord.RichEmbed()
         .setColor("#6457A6")
         .setTitle("Server Information")
         .setThumbnail(message.guild.displayAvatarURL)
-        .addField("Name", message.guild.name)
+        .addField("Name", message.guild.name, true)
+        .addField("Created on", createDate, true)
         .addField("Description", "Discord gaming server for mostly Indian FPS players")
-        .addField("Created on", createDate)
+        .addField('Server region', message.guild.region, true)
+        .addField("Total Members", message.guild.memberCount, true)
         .addField("You joined", joinDate)
-        .addField("Total Members", message.guild.memberCount)
         .setImage("https://i.imgur.com/GzOprvb.jpg")
         .setFooter("\"Main hu Nandu, sabka bandhu\"");
     message.channel.send(serverembed);
