156dabb04026b2f72d271107ebfe9f98824740fd
theHimanshuShekhar <hshekhar.dev@gmail.com>
2018-12-10 04:58:04 +0530

    added neko command

---
+++ commands/cat.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports.run = async (bot, message, args) => {
+
+    fetch('http://aws.random.cat/meow')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let botembed = new Discord.RichEmbed()
+                .setColor("#6457A6")
+                .setImage(data.file)
+            message.channel.send(botembed);
+        });
+}
+
+module.exports.help = {
+    name: "neko"
+}
