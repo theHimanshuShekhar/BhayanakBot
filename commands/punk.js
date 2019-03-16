4057ea8757c16e475a126cd954c8d45cc6f70baf
Himanshu Shekhar <hshekhar.dev@gmail.com>
2019-03-17 04:10:22 +0530

    Update punk.js

--- commands/punk.js
+++ commands/punk.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
-        .setColor("##FF0000")
+        .setColor("#FF0000")
         .setTitle("punklord")
         .setImage("https://cdn.cnn.com/cnnnext/dam/assets/140919185303-tsr-dnt-todd-isis-flames-of-war-00001413-story-top.jpg")
         .setFooter("\"Shut up fuckin' dog!\"");
