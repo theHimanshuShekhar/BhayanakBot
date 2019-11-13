bc28cdf92882bafb65bfcdffaf8a91cd85e35385
punklord <48634203+punklord@users.noreply.github.com>
2019-11-14 03:19:38 +0530

    Update punk.js

--- commands/punk.js
+++ commands/punk.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
         .setColor("#FF69B4")
-        .setTitle("pranklord")
+        .setTitle("punklord")
         .setImage("https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Jihadi_John.jpg/220px-Jihadi_John.jpg")
         .setFooter("\"Takbir My Nigger!"");
     message.channel.send(botembed);
