4138e4e795e2090d30cfefb918c8e0c008e405be
punklord <48634203+punklord@users.noreply.github.com>
2019-11-14 03:33:03 +0530

    Updated punk command (#14)
    * Update punk.js
    
    * Update punk.js
    
    * Update punk.js
    
    * Update punk.js

--- commands/punk.js
+++ commands/punk.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
         .setColor("#FF69B4")
-        .setTitle("pranklord")
-        .setImage("https://www.pegitboard.com/pics/t/297226.jpg")
-        .setFooter("\"PUNK IS GHEY BOI PLAYING DED GAMEZ!\"");
+        .setTitle("punklord")
+        .setImage("https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Jihadi_John.jpg/220px-Jihadi_John.jpg")
+        .setFooter("\"Takbir My Nigger!"");
     message.channel.send(botembed);
 }
 
