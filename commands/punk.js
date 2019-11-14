61c2b73536a4baf45669e50b10c24241a656375b
thehshekhar <hemanshoe.shekhar@gmail.com>
2019-11-14 22:35:29 +0530

    fixed bug

--- commands/punk.js
+++ commands/punk.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
         .setColor("#FF69B4")
         .setTitle("punklord")
         .setImage("https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Jihadi_John.jpg/220px-Jihadi_John.jpg")
-        .setFooter("\"Takbir My Nigger!"");
+        .setFooter("\"Takbir My Nigger!\"");
     message.channel.send(botembed);
 }
 
