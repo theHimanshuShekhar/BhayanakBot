9ed901955acab70b40851a3127f25345c6b907fe
theHimanshuShekhar <hshekhar.dev@gmail.com>
2020-09-14 04:12:26 +0530

    refactored some code and fixed error

--- commands/amongus.js
+++ commands/amongus.js
@@ -1,19 +1,20 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-   if (message.member.voice.channel) {
-            let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-            for (const [memberID, member] of channel.members) {
-                member.voice.setMute(!message.member.voice.serverMute);
-            }
-            if (message.member.voice.serverMute) {
-                message.reply('Find that impostor');
-            } else message.reply('Shhhhh!')
-        } else {
-            message.reply('You need to join a voice channel first!');
-        }
+    voiceChannel = message.member.voiceChannel;
+    if (voiceChannel) {
+        msg = '';
+        voiceChannel.members.map(member => {
+            member.setMute(!member.serverMute);
+            msg = member.serverMute ? 'unmuted' : 'muted';
+        });
+        reply = [voiceChannel.members.size, voiceChannel.members.size == 1 ? " member " : " members ", msg]
+        message.reply(reply.join(""));
+    } else {
+        message.reply('You need to join a voice channel first!');
+    }
 }
 
 module.exports.help = {
-    name: "AU"
+    name: "au"
 }
