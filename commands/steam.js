55d13dc0998e271e0394d7434d70d17191f4a024
theHimanshuShekhar <hshekhar.dev@gmail.com>
2022-01-16 22:33:45 +0530

    add sugar and fix steam command embed

--- commands/steam.js
+++ commands/steam.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const SteamAPI = require("steamapi");
 const steam = new SteamAPI(process.env.STEAMAPIKEY);
+const relativeDate = require("sugar/date/relative");
 
 const personastates = [
   "Offline",
@@ -16,58 +17,71 @@ module.exports.run = async (bot, message, args) => {
   steam
     .resolve("https://steamcommunity.com/id/" + steamuser)
     .then((id) => {
-      steam.getUserSummary(id).then(async (summary) => {
-        // console.log(summary);
-        let bans = await steam.getUserBans(id);
-        let friends = await steam.getUserFriends(id);
-        let level = await steam.getUserLevel(id);
-        let games = await steam.getUserOwnedGames(id);
-        let recent = await steam.getUserRecentGames(id);
+      steam
+        .getUserSummary(id)
+        .then(async (summary) => {
+          // console.log(summary);
+          let bans = await steam.getUserBans(id);
+          let friends = await steam.getUserFriends(id);
+          let level = await steam.getUserLevel(id);
+          let games = await steam.getUserOwnedGames(id);
+          let recent = await steam.getUserRecentGames(id);
 
-        let botembed = new Discord.MessageEmbed()
-          .setColor("#6457A6")
-          .setTitle(summary.nickname + " " + summary.url)
-          .setURL(summary.url)
-          .setImage(summary.avatar.large)
-          .setFooter(
-            "Requested by " +
-              `${message.author.username}` +
-              " at " +
-              new Date().toLocaleString()
-          )
-          .setTimestamp()
-          .addField("State", personastates[summary.personaState], true)
-          .addField("steamID", summary.steamID, true)
-          .addField("Friends", friends.length, true)
-          .addField("Level", level, true)
-          .addField("Primary Group", summary.primaryGroupID, true)
-          .addField("Owned Games", games.length, true);
-        if (bans.daysSinceLastBan) {
-          if (bans.vacBanned) {
-            botembed.addField("VAC Ban", bans.vacBanned, true);
-          }
-          if (bans.communityBanned) {
-            botembed.addField("Community Ban", bans.communityBanned, true);
-          }
-          botembed.addField("Days since last ban", bans.daysSinceLastBan, true);
-        }
-        // botembed.addField('Recent Games', '\u200b')
-        recent
-          .slice(0, 3)
-          .map((game) =>
-            botembed.addField(
-              game.name,
-              (game.playTime / 60).toFixed(0) + " hours"
+          let botembed = new Discord.MessageEmbed()
+            .setColor("#6457A6")
+            .setTitle(summary.nickname + " " + summary.url)
+            .setURL(summary.url)
+            .setImage(summary.avatar.large)
+            .setFooter(
+              "Requested by " +
+                `${message.author.username}` +
+                " at " +
+                new Date().toLocaleString()
             )
-          );
-        let date = new Date();
-        date.setTime(summary.lastLogOff);
-        botembed.setFooter("requested by " + message.author.username);
-        //     .addField('Last Seen', new Date().setSeconds(summary.lastLogOff))
-        //     .addField('created', new Date().setSeconds(summary.created))
-        //
-        message.channel.send(botembed);
-      });
+            .setTimestamp()
+            .addField("State", personastates[summary.personaState], true)
+            .addField("steamID", summary.steamID, true)
+            .addField(
+              "Last Seen",
+              relativeDate(new Date(summary.lastLogOff * 1000)),
+              true
+            )
+            .addField(
+              "created",
+              relativeDate(new Date(summary.created * 1000)),
+              true
+            )
+            .addField("Friends", friends.length, true)
+            .addField("Level", level, true)
+            // .addField("Primary Group", summary.primaryGroupID, true)
+            .addField("Owned Games", games.length);
+          if (bans.daysSinceLastBan) {
+            if (bans.vacBanned) {
+              botembed.addField("VAC Ban", bans.vacBanned, true);
+            }
+            if (bans.communityBanned) {
+              botembed.addField("Community Ban", bans.communityBanned, true);
+            }
+            botembed.addField(
+              "Days since last ban",
+              bans.daysSinceLastBan,
+              true
+            );
+          }
+          recent
+            .slice(0, 3)
+            .map((game) =>
+              botembed.addField(
+                game.name,
+                (game.playTime / 60).toFixed(0) + " hours"
+              )
+            );
+
+          botembed.setFooter("requested by " + message.author.username);
+
+          message.channel.send(botembed);
+        })
+        .catch((err) => console.error(err));
     })
     .catch(() => message.channel.send("Kuch bhi na dal bc."));
 };
