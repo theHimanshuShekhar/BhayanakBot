7de24dc1a9bb89fd9fb53b88c74ed453a2982662
theHimanshuShekhar <hshekhar.dev@gmail.com>
2022-01-18 23:44:23 +0530

    rewrite minecraft server command

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -1,54 +1,25 @@
 const Discord = require("discord.js");
-let { Client, Entity, Schema, Repository } = require("redis-om");
-
-// Create Redis client
-const client = new Client();
-
-async function connect() {
-  if (!client.isOpen()) {
-    try {
-      await client.open(process.env.REDIS_URL);
-    } catch (e) {
-      console.log("Failed to connect to Redis");
-      console.error(e);
-    }
-  }
-}
-
-class MCServer extends Entity {}
-let MCServerSchema = new Schema(
-  MCServer,
-  {
-    name: { type: "string" },
-    url: { type: "string" },
-    description: { type: "string" },
-    addinf: { type: "array" },
-  },
-  {
-    dataStructure: "JSON",
-  }
-);
+const fs = require("fs");
 
 module.exports.run = async (bot, message, args, db) => {
-  await connect();
-  let serverRepository = new Repository(MCServerSchema, client);
   try {
-    await serverRepository.createIndex();
-  } catch (e) {
-    console.log("Index already exists");
-  }
-  let servers = await serverRepository.search().returnAll();
+    let server;
+    const data = fs.readFileSync("serverDetails.json");
+    server = JSON.parse(data);
 
-  servers.forEach((server) => {
-    let embed = new Discord.MessageEmbed()
-      .setColor("#6457A6")
-      .setThumbnail("https://pbs.twimg.com/media/DHLaTWSUwAAfzqX.jpg")
-      .setTitle(server.url)
-      .setDescription(server.description)
-      .setTimestamp()
-      .setFooter(server.name);
-    message.channel.send(embed);
-  });
+    if (server) {
+      let embed = new Discord.MessageEmbed()
+        .setColor("#6457A6")
+        .setThumbnail("https://pbs.twimg.com/media/DHLaTWSUwAAfzqX.jpg")
+        .setTitle(server.url.substr(6))
+        .setDescription(server.description)
+        .setTimestamp()
+        .setFooter(server.name);
+      message.channel.send(embed);
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports.help = {
