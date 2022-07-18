44f6d10ed7bf7c9aec78f88fc308f88af6d6b0b0
theHimanshuShekhar <hshekhar.dev@gmail.com>
2022-07-18 17:38:10 +0530

    added yomomma command

---
+++ commands/yomomma.js
@@ -0,0 +1,26 @@
+//
+
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports.run = async (bot, message, args) => {
+  user = message.mentions.members.first()
+    ? message.mentions.users.first()
+    : message.author;
+
+  fetch("https://yomomma-api.herokuapp.com/jokes")
+    .then((response) => response.json())
+    .then(async (data) => {
+      nickname = (await message.guild.member(user).nickname) || user.username;
+      let botembed = new Discord.MessageEmbed()
+        .setColor("#6457A6")
+        .setTitle(nickname + ", " + data.joke);
+      message.channel.send(botembed);
+    });
+};
+
+module.exports.help = {
+  name: "yomomma",
+  syntax: "yomomma [user]",
+  description: "send yomomma joke to user",
+};
