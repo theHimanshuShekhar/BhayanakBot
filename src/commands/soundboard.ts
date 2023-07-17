f64f0d1f0dfc6d0a10c1f2656953e1b3fe3faa45
theHimanshuShekhar <hshekhar.dev@gmail.com>
2023-07-17 11:09:47 +0530

    Soundboard [WIP]

--- src/commands/soundboard.ts
+++ src/commands/soundboard.ts
@@ -1,5 +1,7 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @ApplyOptions<Command.Options>({
 	name: 'sound',
@@ -7,6 +9,12 @@ import { Command } from '@sapphire/framework';
 })
 export class UserCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry) {
+		// Get all soundboard files from /assets/soundboard
+		// const soundBoardList: string[] = getSounds();
+		// console.log(soundBoardList);
+
+		// getSounds();
+
 		registry.registerChatInputCommand(
 			(builder) =>
 				builder //
@@ -21,3 +29,32 @@ export class UserCommand extends Command {
 		return interaction.reply({ content: 'Hello world!' });
 	}
 }
+
+export function getSounds() {
+	const soundboardDir = path.join(__dirname, '../../assets/soundboard');
+
+	fs.readdir(soundboardDir, (err: NodeJS.ErrnoException | null, files: string[]) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+
+		files.forEach((file: string) => {
+			console.log(file);
+		});
+	});
+}
+
+// Example to refactor | Note- it is message command and not slash command
+// if (!message.member.voiceChannel) {
+//     return errors.userNotInChannel(message);
+//   } else {
+//     message.member.voiceChannel.join().then(connection => {
+//       console.log(`[${message.guild}] ${message.author.username} has issued the ${module.exports.help.name} command.`)
+//       const dispatcher = connection.playFile('./effects/ahh.mp3');
+
+//       dispatcher.on('end', end => {
+//         message.member.voiceChannel.leave();
+//       });
+//     })
+//   }
