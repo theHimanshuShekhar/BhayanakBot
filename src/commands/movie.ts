9d3d5db037d93b3bd94a8c001887572168b09925
theHimanshuShekhar <hshekhar.dev@gmail.com>
2024-08-10 00:20:44 +0530

    upgrade versions and remove broken commands

--- src/commands/movie.ts
+++ src/commands/movie.ts
@@ -1,6 +1,7 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
 import { EmbedBuilder } from 'discord.js';
+import { s } from '@sapphire/shapeshift';
 
 @ApplyOptions<Command.Options>({
 	name: 'imdb',
@@ -20,53 +21,61 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		const searchString = interaction.options.data[0].value?.toString();
+
+		const searchString = s.string.parse(interaction.options.data[0].value?.toString());
 		if (searchString) {
 			const searchParams = new URLSearchParams();
 			searchParams.set('apikey', process.env.OMDB_KEY ? process.env.OMDB_KEY : '');
-			searchParams.set('t', searchString);
+			searchParams.set('t', s.string.parse(searchString));
 
 			fetch('http://www.omdbapi.com/?' + searchParams)
 				.then((response) => response.json())
 				.then((movie) => {
 					if (!movie) return;
-					const botembed = new EmbedBuilder().setTitle(movie.Title).setDescription(movie.Plot).setImage(movie.Poster).addFields(
-						{
-							name: 'Runtime',
-							value: movie.Runtime,
-							inline: true
-						},
-						{
-							name: 'Director',
-							value: movie.Director,
-							inline: true
-						},
-						{
-							name: 'Released',
-							value: movie.Released,
-							inline: true
-						},
-						{
-							name: 'Rated',
-							value: movie.Rated,
-							inline: true
-						},
-						{
-							name: 'Genre',
-							value: movie.Genre,
-							inline: true
-						},
-						{
-							name: 'Country',
-							value: movie.Country,
-							inline: true
-						},
-						{
-							name: 'Actors',
-							value: movie.Actors,
-							inline: true
-						}
-					);
+
+					if (movie.Response && movie.Response === 'False') interaction.reply(movie.Error);
+
+					const botembed = new EmbedBuilder()
+						.setTitle(s.string.parse(movie.Title))
+						.setDescription(s.string.parse(movie.Plot))
+						.setImage(movie.Poster)
+						.addFields(
+							{
+								name: 'Runtime',
+								value: movie.Runtime,
+								inline: true
+							},
+							{
+								name: 'Director',
+								value: movie.Director,
+								inline: true
+							},
+							{
+								name: 'Released',
+								value: movie.Released,
+								inline: true
+							},
+							{
+								name: 'Rated',
+								value: movie.Rated,
+								inline: true
+							},
+							{
+								name: 'Genre',
+								value: movie.Genre,
+								inline: true
+							},
+							{
+								name: 'Country',
+								value: movie.Country,
+								inline: true
+							},
+							{
+								name: 'Actors',
+								value: movie.Actors,
+								inline: true
+							}
+						);
 
 					interaction.reply({ embeds: [botembed] });
 				});
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,28 @@
-import {
-	container,
-	type ChatInputCommandSuccessPayload,
-	type Command,
-	type ContextMenuCommandSuccessPayload,
-	type MessageCommandSuccessPayload
-} from '@sapphire/framework';
+import type { ChatInputCommandSuccessPayload, Command, ContextMenuCommandSuccessPayload, MessageCommandSuccessPayload } from '@sapphire/framework';
+import { container } from '@sapphire/framework';
+import { send } from '@sapphire/plugin-editable-commands';
 import { cyan } from 'colorette';
-import type { APIUser, Guild, User } from 'discord.js';
+import { EmbedBuilder, type APIUser, type Guild, type Message, type User } from 'discord.js';
+import { RandomLoadingMessage } from './constants';
+
+/**
+ * Picks a random item from an array
+ * @param array The array to pick a random item from
+ * @example
+ * const randomEntry = pickRandom([1, 2, 3, 4]) // 1
+ */
+export function pickRandom<T>(array: readonly T[]): T {
+	const { length } = array;
+	return array[Math.floor(Math.random() * length)];
+}
+
+/**
+ * Sends a loading message to the current channel
+ * @param message The message data for which to send the loading message
+ */
+export function sendLoadingMessage(message: Message): Promise<typeof message> {
+	return send(message, { embeds: [new EmbedBuilder().setDescription(pickRandom(RandomLoadingMessage)).setColor('#FF0000')] });
+}
 
 export function logSuccessCommand(payload: ContextMenuCommandSuccessPayload | ChatInputCommandSuccessPayload | MessageCommandSuccessPayload): void {
 	let successLoggerData: ReturnType<typeof getSuccessLoggerData>;
